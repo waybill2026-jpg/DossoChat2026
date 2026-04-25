927a084fbc487651a948be4c3e81e661d231fcbe
waybill2026-jpg <waybill2026@gmail.com>
2026-04-25 11:16:47 +0500

    Update sw.js

--- wwwroot/sw.js
+++ wwwroot/sw.js
@@ -1,41 +1,17 @@
-const CACHE_NAME = "dossochat-v1";
-const assetsToCache = [
-    "/",
-    "/css/site.css",
-    "/js/site.js",
-    "/lib/bootstrap/dist/css/bootstrap.min.css",
-    "/lib/jquery/dist/jquery.min.js",
-    "/assets/icons/logo.png",
-    "/assets/icons/logo-192.png",
-    "/assets/icons/logo-512.png",
-    "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
+const cacheName = 'dosso-v2';
+const staticAssets = [
+  '/',
+  '/Login',
+  '/assets/icons/logo.png'
 ];
 
-// Uygulama yüklendiğinde dosyaları önbelleğe al
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assetsToCache);
-    })
-  );
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
 });
 
-// İnternet olmasa bile önbellekten dosyaları getir
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Eski önbellekleri temizle
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
